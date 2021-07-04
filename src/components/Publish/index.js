9a90d3afedc69ffa86c9a03854f05da9f1e11d89
cloudnets <cloudnets@MacBook-Pro-de-Santiago.local>
2021-07-04 18:03:56 -0500

    chore: agregando header

--- src/components/Publish/index.js
+++ src/components/Publish/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { colors } from '../../constants'
 
-const Publish = () => {
+const Publish = ({ handler }) => {
     return (
-        <TouchableOpacity style={styles.container}>
+        <TouchableOpacity onPress={handler} style={styles.container}>
             <Image style={styles.imgProfile} resizeMode='contain' source={{ uri: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80' }} />
             <Text style={{ color: colors.gray, marginLeft: 8, fontSize: 18, fontWeight: 'bold' }}>Do you want to upload a pet?</Text>
         </TouchableOpacity>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native'
+import React, { useRef } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, FlatList, LogBox, TouchableOpacity, useWindowDimensions, Image, ScrollView } from 'react-native'
 import Header from '../../components/Header';
 import BoxPets from '../../components/BoxPets';
 import CardPet from '../../components/CardPet';
 import Publish from '../../components/Publish';
+import BottomSheet from "react-native-gesture-bottom-sheet";
+import { colors } from '../../constants'
 
 const data = [
     {
@@ -29,8 +31,29 @@ const data = [
     },
 ]
 
-const Home = ({ navigation }) => {
+const HeaderPublish = ({ bottomSheet }) => {
+    return (
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <TouchableOpacity onPress={() => bottomSheet.current.close()}>
+                    <Image style={{ width: 30, height: 30 }} resizeMode='contain' source={{ uri: 'https://image.flaticon.com/icons/png/512/507/507257.png' }} />
+                </TouchableOpacity>
+                <Text style={{ fontSize: 18, marginLeft: 10 }}>Crear publicacion</Text>
+            </View>
+            <View>
+                <TouchableOpacity style={{ backgroundColor: colors.primary, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 10 }}>
+                    <Text style={{ color: 'white', fontWeight: 'bold' }}>Publicar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
 
+const Home = ({ navigation }) => {
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+    const windowWidth = useWindowDimensions().width;
+    const windowHeight = useWindowDimensions().height;
+    const bottomSheet = useRef();
     const renderItem = ({ item }) => (
         <CardPet navigation={navigation} handler={() => navigation.navigate('InfoPet')} name={item.name} time={item.time} img={item.img} imgPet={item.imgPet} />
     )
@@ -40,7 +63,7 @@ const Home = ({ navigation }) => {
             <View style={styles.container}>
                 <Header />
                 <View style={{ marginTop: 20 }}>
-                    <Publish />
+                    <Publish handler={() => bottomSheet.current.show()} />
                 </View>
                 <View style={{ marginTop: 20 }}>
                     <BoxPets />
@@ -53,6 +76,14 @@ const Home = ({ navigation }) => {
                         keyExtractor={item => item.id}
                     />
                 </View>
+                <BottomSheet draggable={false} ref={bottomSheet} height={windowHeight - 50}>
+                    <View style={{ flex: 1, paddingHorizontal: 20, paddingVertical: 20 }}>
+                        <HeaderPublish bottomSheet={bottomSheet} />
+                        <ScrollView style={{ flex: 1 }}>
+                            <Text>1</Text>
+                        </ScrollView>
+                    </View>
+                </BottomSheet>
             </View>
         </SafeAreaView>
     );
