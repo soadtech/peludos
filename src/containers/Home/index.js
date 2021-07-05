1d1a72f88cd1a8ffaa386940647ed3cab70520c4
cloudnets <cloudnets@MacBook-Pro-de-Santiago.local>
2021-07-04 20:13:26 -0500

    chore: agregando componente para crear una mascota

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react';
-import { View, Text, SafeAreaView, StyleSheet, FlatList, LogBox, TouchableOpacity, useWindowDimensions, Image, ScrollView } from 'react-native'
+import React, { useRef, useState } from 'react';
+import { View, TextInput, Text, SafeAreaView, StyleSheet, FlatList, LogBox, TouchableOpacity, useWindowDimensions, Image, ScrollView } from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
 import Header from '../../components/Header';
 import BoxPets from '../../components/BoxPets';
 import CardPet from '../../components/CardPet';
@@ -31,6 +32,15 @@ const data = [
     },
 ]
 
+const options = {
+    title: 'Select Avatar',
+    customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+    storageOptions: {
+        skipBackup: true,
+        path: 'images',
+    },
+};
+
 const HeaderPublish = ({ bottomSheet }) => {
     return (
         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
@@ -58,6 +68,47 @@ const Home = ({ navigation }) => {
         <CardPet navigation={navigation} handler={() => navigation.navigate('InfoPet')} name={item.name} time={item.time} img={item.img} imgPet={item.imgPet} />
     )
 
+    const [filepath, setFilePath] = useState({
+        data: '',
+        uri: ''
+    });
+    const [fileData, setFileData] = useState('')
+    const [fileUri, setFilUri] = useState([])
+
+    const chooseImage = () => {
+        let options = {
+            title: 'Select Image',
+            customButtons: [
+                { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+            ],
+            storageOptions: {
+                skipBackup: true,
+                path: 'images',
+            },
+        };
+        launchImageLibrary(options, (response) => {
+            console.log('Response = ', response.assets[0].uri);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+                alert(response.customButton);
+            } else {
+                const source = { uri: response.assets[0].uri };
+
+                // You can also display the image using data:
+                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+                // alert(JSON.stringify(response));s
+                console.log('response', source);
+                setFilePath(response)
+                setFileData(response.data)
+                setFilUri([...fileUri, source])
+            }
+        });
+    }
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={styles.container}>
@@ -79,9 +130,68 @@ const Home = ({ navigation }) => {
                 <BottomSheet draggable={false} ref={bottomSheet} height={windowHeight - 50}>
                     <View style={{ flex: 1, paddingHorizontal: 20, paddingVertical: 20 }}>
                         <HeaderPublish bottomSheet={bottomSheet} />
-                        <ScrollView style={{ flex: 1 }}>
-                            <Text>1</Text>
-                        </ScrollView>
+                        <View style={{ flex: 1 }}>
+                            <ScrollView style={{ flex: 1 }}>
+                                <View style={{ marginTop: 30 }}>
+                                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Datos basicos</Text>
+
+                                    <View style={{ flexDirection: 'row', flex: 1 }}>
+                                        <View style={{ marginTop: 10, flex: 0.5 }}>
+                                            <Text style={{ fontSize: 18, marginBottom: 5 }}>Nombre</Text>
+                                            <TextInput style={{ width: '100%', paddingHorizontal: 5, height: 40, borderRadius: 15, backgroundColor: colors.gray }} />
+                                        </View>
+                                        <View style={{ width: 10 }}></View>
+                                        <View style={{ marginTop: 10, flex: 0.5 }}>
+                                            <Text style={{ fontSize: 18, marginBottom: 5 }}>Nombre</Text>
+                                            <TextInput style={{ width: '100%', paddingHorizontal: 5, height: 40, borderRadius: 15, backgroundColor: colors.gray }} />
+                                        </View>
+                                    </View>
+                                </View>
+
+                                <View style={{ marginTop: 30 }}>
+                                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Genero</Text>
+
+                                    <View style={{ flexDirection: 'row', flex: 1 }}>
+                                        <View style={{ width: '100%', justifyContent: 'center', flex: 0.5, paddingHorizontal: 5, height: 40, borderRadius: 15, backgroundColor: colors.gray }}>
+                                            <Text style={{ textAlign: 'center' }}>Male</Text>
+                                        </View>
+                                        <View style={{ width: 10 }}></View>
+                                        <View style={{ width: '100%', justifyContent: 'center', flex: 0.5, paddingHorizontal: 5, height: 40, borderRadius: 15, backgroundColor: colors.gray }}>
+                                            <Text style={{ textAlign: 'center' }}>Famale</Text>
+                                        </View>
+                                    </View>
+                                </View>
+
+                                <View style={{ marginTop: 30 }}>
+                                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Descripcion</Text>
+
+                                    <View style={{ flexDirection: 'row', flex: 1 }}>
+                                        <TextInput numberOfLines={4} multiline style={{ width: '100%', paddingHorizontal: 5, height: 70, borderRadius: 15, backgroundColor: colors.gray }} />
+                                    </View>
+                                </View>
+
+                                <View style={{ marginTop: 30 }}>
+                                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Fotos</Text>
+
+                                    <View style={{ flex: 1 }}>
+                                        <TouchableOpacity onPress={fileUri.length <= 3 ? chooseImage : undefined} style={{ paddingHorizontal: 10, backgroundColor: fileUri.length <= 3 ? colors.primary : colors.primaryLight, paddingVertical: 15, borderRadius: 10 }}>
+                                            <Text style={{ color: 'white' }}>Select image</Text>
+                                        </TouchableOpacity>
+                                        <View style={styles.list}>
+                                            {fileUri.map(uri => {
+                                                return (
+                                                    <View key={uri.uri} style={styles.listadoItem}>
+                                                        <Image style={styles.mejores} resizeMode='contain' source={{ uri: uri.uri }} />
+                                                    </View>
+                                                )
+                                            })}
+
+                                        </View>
+
+                                    </View>
+                                </View>
+                            </ScrollView>
+                        </View>
                     </View>
                 </BottomSheet>
             </View>
@@ -94,6 +204,18 @@ const styles = StyleSheet.create({
         paddingHorizontal: 18,
         paddingVertical: 18,
         backgroundColor: '#f3f3f3'
+    },
+    list: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    mejores: {
+        width: 200,
+        height: 100,
+        marginVertical: 5
+    },
+    listadoItem: {
+        flexBasis: '49%'
     }
 })
 export default Home;
